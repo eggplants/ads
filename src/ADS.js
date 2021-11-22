4d06079456e43dd9232e0b1681176146a17290ca
eggplants <w10776e8w@yahoo.co.jp>
2021-11-22 15:06:13 +0900

    fix

--- src/ADS.js
+++ src/ADS.js
@@ -1,17 +1,17 @@
 ADS = [
   `
-    <a href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F" rel="nofollow">
+    <a target="_blank" href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+6I9N5&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_6I9N5%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F" rel="nofollow">
     <img src="http://hbb.afl.rakuten.co.jp/hsb/0ea7f9a4.79280dcb.0ea7f99d.1ac92fca/153145/" border="0"></a>
     <img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HP2ZF+1IRWFM+2HOM+6I9N5" alt="">
   `,
 
   `
-    <a href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+6K735&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_6K735%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F" rel="nofollow">
+    <a target="_blank" href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+6K735&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4779e.5d30c5ba.0eb4779f.b871e4e3%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_6K735%3Fpc%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Ftravel.rakuten.co.jp%252F" rel="nofollow">
     <img src="http://hbb.afl.rakuten.co.jp/hsb/0ea7f9c8.514613d5.0ea7f99d.1ac92fca/153145/" border="0"></a>
     <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3HP2ZF+1IRWFM+2HOM+6K735" alt="">
   `,
   `
-    <a href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+7K7DT&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4cf04.fd65a65c.0eb4cf05.fa3f041c%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_7K7DT%3Fpc%3Dhttp%253A%252F%252Fgora.golf.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F" rel="nofollow">
+    <a  target="_blank" href="https://rpx.a8.net/svt/ejp?a8mat=3HP2ZF+1IRWFM+2HOM+7K7DT&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0eb4cf04.fd65a65c.0eb4cf05.fa3f041c%2Fa21112289110_3HP2ZF_1IRWFM_2HOM_7K7DT%3Fpc%3Dhttp%253A%252F%252Fgora.golf.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F" rel="nofollow">
     <img src="http://hbb.afl.rakuten.co.jp/hsb/0eb47d91.538b468b.0eb47d8d.fb51150d/" border="0"></a>
     <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3HP2ZF+1IRWFM+2HOM+7K7DT" alt="">
   `,
