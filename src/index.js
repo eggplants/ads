87b47e223fa02dbaabffbd2e1003dd90bec814fa
eggplants <w10776e8w@yahoo.co.jp>
2021-11-22 16:21:46 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ var main = () => {
     "scroll",
     function () {
       let d = document.documentElement;
-      if (d.scrollTop + d.clientHeight >= d.scrollHeight - 300) {
+      if (d.scrollTop + d.clientHeight >= d.scrollHeight - 1000) {
         addAds(30);
       }
     },
